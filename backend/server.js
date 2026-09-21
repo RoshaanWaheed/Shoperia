f0001fe0162e355c05d2d22e6428dee23610f0bc
Roshaan Zaib <m.roshaanzaib@gmail.com>
2026-09-21 09:25:22 +0500

    changes for vercel deployement

--- backend/server.js
+++ backend/server.js
@@ -2,48 +2,36 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import cors from 'cors';
+
 import authRoutes from './routes/authRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
-import errorMiddleware from './middleware/errorMiddleware.js';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
+import errorMiddleware from './middleware/errorMiddleware.js';
 
 dotenv.config();
-
 
 connectDB();
 
 const app = express();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5000'],
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5000'
+  ],
   credentials: true
 }));
-app.use(express.json());
 
+app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/upload', uploadRoutes);
-
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/dist')));
-  app.get(/(.*)/, (req, res) =>
-  res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'))
-);
-}
-
 
 app.use(errorMiddleware);
 
